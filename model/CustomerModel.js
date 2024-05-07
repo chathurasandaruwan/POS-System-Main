c5079a0c709015d571947dda7f39e0cb09fbd5d4
chathura <chathurasandaruwan122@gmail.com>
2024-05-07 17:25:21 +0530

    create CustomerModel.js

---
+++ model/CustomerModel.js
@@ -0,0 +1,41 @@
+class CustomerModel{
+
+    constructor(customerId,customerName,customerAdd,customerSalary) {
+        this._customerId = customerId;
+        this._customerName = customerName;
+        this._customerAdd = customerAdd;
+        this._customerSalary = customerSalary;
+    }
+
+    get customerId() {
+        return this._customerId;
+    }
+
+    set customerId(value) {
+        this._customerId = value;
+    }
+
+    get customerName() {
+        return this._customerName;
+    }
+
+    set customerName(value) {
+        this._customerName = value;
+    }
+
+    get customerAdd() {
+        return this._customerAdd;
+    }
+
+    set customerAdd(value) {
+        this._customerAdd = value;
+    }
+
+    get customerSalary() {
+        return this._customerSalary;
+    }
+
+    set customerSalary(value) {
+        this._customerSalary = value;
+    }
+}
