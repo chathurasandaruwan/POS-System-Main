a1304886b9c0ad9fec2671453a6ecfe4749c040f
chathura <chathurasandaruwan122@gmail.com>
2024-05-09 13:26:24 +0530

    load customer data using select index in PlaceOrder.js

--- controller/PlaceOrder.js
+++ controller/PlaceOrder.js
@@ -2,23 +2,26 @@ import {CustomerAr} from "../db/db.js";
 
  export function refresh() {
   console.log("refreshed")
-  $('#inputState').empty();
+  $('#customer_inputState').empty();
   for (let i = 0; i < CustomerAr.length; i++) {
-   $('#inputState').append($('<option>', {
+   $('#customer_inputState').append($('<option>', {
     value: i,
     text: CustomerAr[i].customerId
    }));
   }
  }
 
-$("#inputState").on('click','option',function (){
+$("#customer_inputState").on('click','option',function (){
  let index = $(this).index();
  // console.log(CustomerAr[index]);
  let customerId = CustomerAr[index].customerId;
  let customerName = CustomerAr[index].customerName;
  let customerAdd = CustomerAr[index].customerAdd;
  let customerSalary = CustomerAr[index].customerSalary;
+ $("#C_id").val(customerId);
+ $("#C_name").val(customerName);
+ $("#C_salary").val(customerSalary);
+ $("#C_address").val(customerAdd);
 
- console.log(customerSalary);
 });
 
