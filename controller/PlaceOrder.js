39711a3f42c5cb0eb7ed8f99501e106708df0c2e
chathura <chathurasandaruwan122@gmail.com>
2024-05-12 11:52:18 +0530

    try to create delete btn PlaceOrder.js

--- controller/PlaceOrder.js
+++ controller/PlaceOrder.js
@@ -2,6 +2,7 @@ import {CustomerAr,PlaceOrderAr} from "../db/db.js";
 import {ItemAr} from "../db/db.js";
 import PlaceOrderModel from "../model/PlaceOrderModel.js";
 import {reloadItemTable} from "./Item.js";
+var recordIndex;
 export function refreshCustomers() {
   $('#customer_inputState').empty();
   for (let i = 0; i < CustomerAr.length; i++) {
@@ -54,44 +55,53 @@ $("#addToCart_btn").on('click' , () => {
  let qtyVal = $("#qty").val();
  let qtyOHVal = $("#qtyOH").val();
  let itemTotal = itemPriceVal*qtyVal;
+let btnText =  $("#addToCart_btn").text();
 
+if (btnText === "AddToCart"){
+ if (qtyOHVal > 0) {
+  let itemIndex = ItemAr.findIndex(item => item.item_code === itemCodeVal);
+  if (itemIndex !== -1) ItemAr[itemIndex].item_qty -= qtyVal;
 
-if (qtyOHVal > 0) {
- let itemIndex = ItemAr.findIndex(item => item.item_code === itemCodeVal);
- if (itemIndex !== -1) ItemAr[itemIndex].item_qty -= qtyVal;
-
- let found =false;
- for (let i = 0; i < PlaceOrderAr.length; i++) {
-  if (PlaceOrderAr[i].itemCode === itemCodeVal) {
-   var placeOrderObj = PlaceOrderAr[i];
-   placeOrderObj.qty = Number(qtyVal) + Number(PlaceOrderAr[i].qty);
-   placeOrderObj.total = Number(itemTotal) + Number(PlaceOrderAr[i].total);
-   loadTable();
-   reloadItemTable();
-   clearItemSelectInputs();
-   found = true;
-   break;
+  let found =false;
+  for (let i = 0; i < PlaceOrderAr.length; i++) {
+   if (PlaceOrderAr[i].itemCode === itemCodeVal) {
+    var placeOrderObj = PlaceOrderAr[i];
+    placeOrderObj.qty = Number(qtyVal) + Number(PlaceOrderAr[i].qty);
+    placeOrderObj.total = Number(itemTotal) + Number(PlaceOrderAr[i].total);
+    loadTable();
+    reloadItemTable();
+    clearItemSelectInputs();
+    found = true;
+    break;
+   }
   }
+  if (!found){
+   let placeOrderDetails = new PlaceOrderModel(itemCodeVal, itemNameVal, itemPriceVal, qtyVal, itemTotal);
+   PlaceOrderAr.push(placeOrderDetails);
+   reloadItemTable();
+   loadTable();
+   clearItemSelectInputs();
+  }
+ }else {
+  console.log("empty qty");
  }
- if (!found){
-  let placeOrderDetails = new PlaceOrderModel(itemCodeVal, itemNameVal, itemPriceVal, qtyVal, itemTotal);
-  PlaceOrderAr.push(placeOrderDetails);
-  reloadItemTable();
-  loadTable();
-  clearItemSelectInputs();
- }
-}else {
- console.log("empty qty");
-}
  let sumTot = 0;
  for (let i = 0; i < PlaceOrderAr.length; i++) {
   sumTot += PlaceOrderAr[i].total;
  }
-$("#lbl-total").text(sumTot);
+ $("#lbl-total").text(sumTot);
  let discountPre = $('#discount').val();
  let subTotal = sumTot -(sumTot * (discountPre/100));
 
-$("#lbl-subTotal").text(subTotal);
+ $("#lbl-subTotal").text(subTotal);
+
+}else {
+ PlaceOrderAr.splice(recordIndex,1);
+ loadTable();
+ clearItemSelectInputs();
+ $("#addToCart_btn").text("AddToCart");
+}
+
 });
 
 function clearItemSelectInputs() {
@@ -105,12 +115,32 @@ function loadTable() {
  $('#placeOrder-table').empty();
  PlaceOrderAr.map((item,index) =>{
   var record=`<tr>
-            <td>${item.itemCode}</td>
-            <td>${item.itemName}</td>
-            <td>${item.price}</td>
-            <td>${item.qty}</td>
-            <td>${item.total}</td>
+            <td id="itemCodeValue">${item.itemCode}</td>
+            <td id="itemNameValue">${item.itemName}</td>
+            <td id="priceValue">${item.price}</td>
+            <td id="qtyValue">${item.qty}</td>
+            <td id="total">${item.total}</td>
         </tr>`
   $('#placeOrder-table').append(record);
  });
 }
+
+$("#placeOrder-table").on('click','tr',function (){
+ let index = $(this).index();
+ recordIndex = index;
+
+ let itemCodeValue = $(this).find("#itemCodeValue").text();
+ let itemNameValue = $(this).find("#itemNameValue").text();
+ let priceValue = $(this).find("#priceValue").text();
+ let qtyValue = $(this).find("#qtyValue").text();
+
+ $("#itemCode").val(itemCodeValue);
+ $("#itemName").val(itemNameValue);
+ $("#price").val(priceValue);
+ $("#qty").val(qtyValue);
+
+ $("#addToCart_btn").text("Remove");
+ $("#addToCart_btn").css({backgroundColor : "red", color: "white" });
+
+
+});
